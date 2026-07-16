79873ec859dd6901f9a658d84edd4005fb7c15f9
Carmelia Ong <carmeliaong@OSXLAP07357.local>
2026-07-16 00:54:53 -0400

    Set button sound gain to 0.298

--- app/page.tsx
+++ app/page.tsx
@@ -444,7 +444,7 @@ export default function Home() {
       const gain = context.createGain();
       oscillator.type = "square";
       oscillator.frequency.value = frequency;
-      gain.gain.setValueAtTime(0.336, now);
+      gain.gain.setValueAtTime(0.298, now);
       gain.gain.exponentialRampToValueAtTime(0.0001, now + 0.075);
       oscillator.connect(gain).connect(context.destination);
       oscillator.start(now);
